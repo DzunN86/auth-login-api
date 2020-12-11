7fd36f87de9ea362be1836178998a8887b6ed82e
Dzun <nurroinz@gmail.com>
2020-12-11 22:35:39 +0700

    test auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,5 +8,8 @@ module.exports.signup_post = (req, res) => {
     res.send('signup');
 }
 module.exports.login_post = (req, res) => {
+    const { email, password } = req.body;
+
+    
     res.send('user login');
 }
