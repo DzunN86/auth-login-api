78bca0fcc3f87415a530135f6f29254ee47bef34
Dzun <nurroinz@gmail.com>
2020-12-12 19:01:09 +0700

    user model

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,26 @@
+const User = require('../models/User')
+
 module.exports.signup_get = (req, res) => {
     res.render('signup');
 }
 module.exports.login_get = (req, res) => {
     res.render('login');
 }
-module.exports.signup_post = (req, res) => {
-    res.send('signup');
-}
-module.exports.login_post = (req, res) => {
+module.exports.signup_post = async (req, res) => {
     const { email, password } = req.body;
 
+    try {
+        const user = await User.create({ email, password });
+        res.status(201).json(user);
+    } catch (err) {
+        console.log(err);
+        res.status(400).send('Error, User not created!');
+    }
+}
+module.exports.login_post = async (req, res) => {
+    const { email, password } = req.body;
+
+    console.log(email, password)
     
     res.send('user login');
 }
