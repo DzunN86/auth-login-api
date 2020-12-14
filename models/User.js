3d46dc0daf7f0ddbfc443e3e50d1a700e5cfa195
Dzun <nurroinz@gmail.com>
2020-12-14 22:08:26 +0700

    hash

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -14,7 +15,15 @@ const userSchema = new mongoose.Schema({
         required: [true, 'Please enter an password'],
         minlength: [6, 'Minimum password lenght is 6 characters']
     }
-})
+});
+
+//  fire a function before  doc saved to db
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
 
 const User = mongoose.model('user', userSchema);
 
