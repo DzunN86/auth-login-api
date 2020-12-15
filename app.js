82f46ae90ad668cfcca39b766c67cfee3b1c22fc
Dzun <nurroinz@gmail.com>
2020-12-15 12:08:58 +0700

    use  cookieParser

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const  cookieParser = require('cookie-parser');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
