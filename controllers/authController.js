8ae9326e08277e1d9e9dadfbec6427073d4c8e3e
Dzun <nurroinz@gmail.com>
2020-12-15 12:08:41 +0700

    new user sign up and create token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User')
+const jwt = require('jsonwebtoken')
 
 // Handle errors
 const handleErrors = (err) => {
@@ -21,6 +22,13 @@ const handleErrors = (err) => {
     return errors;
 }
 
+const maxAge = 3 * 24 * 60 * 60 ;
+const createToken = (id) => {
+    return jwt.sign({ id }, 'vj code', {
+        expiresIn: maxAge,
+    })
+}
+
 module.exports.signup_get = (req, res) => {
     res.render('signup');
 }
@@ -32,12 +40,16 @@ module.exports.signup_post = async (req, res) => {
 
     try {
         const user = await User.create({ email, password });
-        res.status(201).json(user);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+        res.status(201).json({user: user._id });
     } catch (err) {
         const errors = handleErrors(err);
         res.status(400).json({ errors })
     }
 }
+
+
 module.exports.login_post = async (req, res) => {
     const { email, password } = req.body;
 
