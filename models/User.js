59c425b9c26f10433d7396474c2c425016018e66
Dzun <nurroinz@gmail.com>
2020-12-16 23:04:53 +0700

    fixing error

--- models/User.js
+++ models/User.js
@@ -25,17 +25,17 @@ userSchema.pre('save', async function (next) {
 });
 
 // static method to login user
-userSchema.static.login = async function (emai, password) {
-    const user = await this.findOne({ email});
+userSchema.statics.login = async function(email, password) {
+    const user = await this.findOne({ email });
     if (user) {
-        const auth = await bcrypt.compare(password, user.password);
-        if (auth) {
-            return user;
-        }
-        throw Error('Incorect password');
+      const auth = await bcrypt.compare(password, user.password);
+      if (auth) {
+        return user;
+      }
+      throw Error('incorrect password');
     }
-    throw Error('Incorect email');
-}
+    throw Error('incorrect email');
+  };
 
 
 const User = mongoose.model('user', userSchema);
