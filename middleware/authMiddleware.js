b1e871708b83b1d907894ba595dcb15227ec2af2
Dzun <nurroinz@gmail.com>
2020-12-18 18:00:17 +0700

    checking the current user

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,22 +1,44 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
-const requireAtuh = (req, res, next) => {
+const requireAuth = (req, res, next) => {
   const token = req.cookies.jwt;
 
-  // check JWT exists & is verified
+  // check json web token exists & is verified
   if (token) {
-    jwt.verify(token, "vj86", (err, decodedToken) => {
+    jwt.verify(token, 'net ninja secret', (err, decodedToken) => {
       if (err) {
         console.log(err.message);
-        res.redirect("/login");
+        res.redirect('/login');
       } else {
         console.log(decodedToken);
         next();
       }
     });
   } else {
-    res.redirect("/login");
+    res.redirect('/login');
   }
 };
 
-module.exports = { requireAtuh };
+// check current user
+const checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, 'net ninja secret', async (err, decodedToken) => {
+      if (err) {
+        res.locals.user = null;
+        next();
+      } else {
+        let user = await User.findById(decodedToken.id);
+        res.locals.user = user;
+        next();
+      }
+    });
+  } else {
+    res.locals.user = null;
+    next();
+  }
+};
+
+
+module.exports = { requireAuth, checkUser };
